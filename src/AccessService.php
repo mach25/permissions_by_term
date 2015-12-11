daa9bc1060e906165f71a55d5b0b2172e8097761
jepster <4004ar@gmail.com>
2015-12-11 17:52:51 +0100

    Added new form state handling by service for user names form field.

---
+++ src/AccessService.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\permissions_by_term\AccessService.
+ */
+
+namespace Drupal\permissions_by_term;
+
+use Drupal\Core\Database\Driver\mysql\Connection;
+use Drupal\Core\Entity\EntityManager;
+use Drupal\Core\Form\FormState;
+
+/**
+ * Class AccessService.
+ *
+ * @package Drupal\permissions_by_term
+ */
+class AccessService implements AccessServiceInterface {
+
+  /**
+   * Drupal\Core\Database\Driver\mysql\Connection definition.
+   *
+   * @var Drupal\Core\Database\Driver\mysql\Connection
+   */
+  protected $oDatabase;
+  protected $oFormState;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(Connection $database, FormState $oFormState) {
+    $this->oDatabase = $database;
+    $this->oFormState = $oFormState;
+  }
+
+  /**
+   * Checks if the submitted users are existing.
+   *
+   * @param array $aAllowedUsers An array with usernames.
+   *
+   * @return null
+   */
+  public function checkIfUsersExists($aAllowedUsers) {
+
+    foreach ($aAllowedUsers as $sUserName) {
+
+      $aUserId = \Drupal::entityQuery('user')
+        ->condition('name', $sUserName)
+        ->execute();
+
+      if (empty($aUserId)) {
+
+        $this->oFormState->setErrorByName('access][user', t('The user %user_name does not exist.',
+          array('%user_name' => $sUserName)));
+
+      }
+
+    }
+
+  }
+
+}
---
+++ src/AccessServiceInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\permissions_by_term\AccessServiceInterface.
+ */
+
+namespace Drupal\permissions_by_term;
+
+/**
+ * Interface AccessServiceInterface.
+ *
+ * @package Drupal\permissions_by_term
+ */
+interface AccessServiceInterface {
+
+
+}
