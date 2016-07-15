eea618a03bd74aa8c51189b1bb4df97312da31a6
Peter Majmesku <p.majmesku@gmail.com>
2016-07-15 12:09:38 +0200

    Added some example code from the paragraphs module for test.

--- tests/src/Kernel/SelectTermTest.php
+++ tests/src/Kernel/SelectTermTest.php
@@ -33,7 +33,32 @@ class SavePermissionsTest extends KernelTestBase {
    * Tests if an editor has access to a term on node edit form.
    */
   public function testTermFormAccess() {
+    // Add a paragraph field to the article.
+    $field_storage = FieldStorageConfig::create(array(
+      'field_name' => 'secured_areas',
+      'entity_type' => 'term_relation',
+      //'type' => 'entity_reference_revisions',
+    ));
+    $field_storage->save();
+    $field = FieldConfig::create(array(
+      'field_storage' => $field_storage,
+      'bundle' => 'test_pbt',
+    ));
+    $field->save();
 
+    // Add a paragraph field to the article.
+    $field_storage = FieldStorageConfig::create(array(
+      'field_name' => 'node_pbt_field',
+      'entity_type' => 'node',
+      'type' => 'entity_reference_revisions',
+      'cardinality' => '-1',
+    ));
+    $field_storage->save();
+    $field = FieldConfig::create(array(
+      'field_storage' => $field_storage,
+      'bundle' => 'article',
+    ));
+    $field->save();
   }
 
 }
